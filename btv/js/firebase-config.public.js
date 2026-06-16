5b9232b931006a0e6530027c6ff99de0004e81a8
moonlover20 <focusing11865@naver.com>
2026-06-17 03:33:46 +0900

    Add Firebase configuration for leaderboard

---
+++ btv/js/firebase-config.public.js
@@ -0,0 +1,8 @@
+window.LEADERBOARD_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCkOwVjreuNFq3QKVTGYtpcw_acmJfMh08",
+  authDomain: "mydungeongame.firebaseapp.com",
+  projectId: "mydungeongame",
+  storageBucket: "mydungeongame.firebasestorage.app",
+  messagingSenderId: "816226458104",
+  appId: "1:816226458104:web:6f6fcb21b1664e34ee9e0d"
+};
